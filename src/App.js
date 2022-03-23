434c2c5bce7b4410c5b9b8e6ff6693c9d888917c
Terry Brooks, Jr <T.A.BrooksAnalytics@gmail.com>
2022-03-23 00:30:20 -0500

    Section 2- UL Rendered

--- src/App.js
+++ src/App.js
@@ -3,38 +3,42 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      < Header name="Billy's" />
-      < Main adjective='Amazing' />
-      < Footer year={(new Date().getFullYear())} />
+      <Header name="Terry" />
+      <Main adjective="Amazing" breeds={breeds} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
 
-
-function Header(props){
-  console.log(props)
+function Header(props) {
+  console.log(props);
   return (
-  <header>
-    <h1>{props.name} Pet Store</h1>
-  </header>
-  )};
-
+    <header>
+      <h1>{props.name}'s Pet Store</h1>
+    </header>
+  );
+}
 
 function Main(props) {
   return (
-    <section> 
-    <h3>{props.adjective} Puppies in Town</h3>
+    <section>
+      <h3>{props.adjective} Puppies in Town</h3>
+      <h4> We have the following breeds:</h4>
+      <ul stlye={{ listStyleType: "none" }}>
+        {props.breeds.map((breed) => (
+          <li>{breed}</li>
+        ))}
+      </ul>
     </section>
-  )
-};
+  );
+}
 
-function Footer(props){
+function Footer(props) {
   return (
     <footer>
       <h3>Get'em before they are gone in {props.year}</h3>
     </footer>
-    )
+  );
 }
 
-const breeds = ['Pit Bull','Rotwiler', 'German Shepherd', 'Miniture Schauzer']
-{props.breeds.map(breed) => console.log(breed));}
+const breeds = ["Pit Bull", "Rotwiler", "German Shepherd", "Miniture Schauzer"];
